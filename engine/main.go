e6af05366ecbf1cb55c91a252f9b82d56bc183e1
hanshunshun <hanshunshun@pinming.cn>
2020-05-18 14:39:43 +0800

    version

--- engine/main.go
+++ engine/main.go
@@ -4,7 +4,7 @@ func main() {
 	cmd := parseCmd()
 
 	if cmd.versionFlag {
-		println("version 0.0.1")
+		println("version 1.0.0")
 	} else if cmd.helpFlag || cmd.class == "" {
 		printUsage()
 	} else {
